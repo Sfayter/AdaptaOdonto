445214c75fcfdc171e878ecd140caa131fb64d05
Pedro Melo <pm5679113@gmail.com>
2024-11-20 21:22:36 -0300

    Tamenho do login alterado

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,13 +6,13 @@ import Erro from '../../components/Erro'
 
 export default function Login() {
 
-    const url = process.env.REACT_APP_API_URL+"/login"
+    const url = process.env.REACT_APP_API_URL + "/login"
     const navigate = useNavigate()
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [erro, setErro] = useState('')
-    async function logar(){
-        await axios.post(url+"/auth", {email, senha})
+    async function logar() {
+        await axios.post(url + "/auth", { email, senha })
             .then((res) => {
                 let token = res.data.token
                 localStorage.setItem('token', token)
@@ -27,19 +27,20 @@ export default function Login() {
     return (
         <div className='login-page'>
             <section className='login'>
-                <form onSubmit={e=> {
+                <img className='logo-odonto' alt="" src="/assets/images/AdaptaOdonto_logo.png" />
+                <form onSubmit={e => {
                     e.preventDefault()
                     logar()
                 }} className='labels'>
-                <img className='logo-odonto' alt="" src="/assets/images/AdaptaOdonto_logo.png"/>
+
                     <input value={email} onChange={e => setEmail(e.target.value)} type="email" id="email" name="email" required placeholder='Email'></input>
                     <input value={senha} onChange={e => setSenha(e.target.value)} type="password" id="password" name="password" required placeholder='Senha'></input>
                     <button className='bt-entrar' type="submit">Login</button>
                 </form>
             </section>
-            {erro.length > 0?
-                <Erro close={() => setErro('')} mensagem={erro}/>
-                :''
+            {erro.length > 0 ?
+                <Erro close={() => setErro('')} mensagem={erro} />
+                : ''
             }
         </div>
     )
